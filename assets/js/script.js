defa0d2ddc92901c3743ac02307d98de21131c1a
pasqualedimaro <pakydm15@gmail.com>
2025-06-18 11:56:18 +0200

    complete main css and photocard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
 //provo a creare un div per ogni foto
-const container = document.getElementById('photo-container');
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('photo-container');
 
-fetch('https://lanciweb.github.io/demo/api/pictures/')
-    .then(response => response.json())
-    .then(pictures => {
-        pictures.forEach(pic => {
-            const card = document.createElement('div');
-            card.innerHTML = `
-                <img src="${pic.url}" alt="${pic.title}">
-                <h2>${pic.title}</h2>
-                <p>${pic.date}</p>
-            `;
-            container.appendChild(card);
+    fetch('https://lanciweb.github.io/demo/api/pictures/')
+        .then(response => response.json())
+        .then(pictures => {
+            pictures.forEach(pic => {
+                const card = document.createElement('div');
+                card.className = 'photo-card';
+                card.innerHTML = `
+    <img src="${pic.url}" alt="${pic.title}">
+    <div class="photo-info">
+        <h2 class="photo-title">${pic.title}</h2>
+        <p class="photo-date">${pic.date}</p>
+    </div>
+                `;
+                container.appendChild(card);
+            });
         })
         .catch(error => console.error('Errore nel fetch:', error));
-    });
+});
